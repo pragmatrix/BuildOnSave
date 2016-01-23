32eba1388f58683ec36c0a2b0767810f00468756
Armin Sander <armin@replicator.org>
2016-01-23 17:52:06 +0100

    removed unused usings

--- BuildOnSave/BackgroundBuild.cs
+++ BuildOnSave/BackgroundBuild.cs
@@ -1,11 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Diagnostics;
 using System.Globalization;
 using System.IO;
 using System.Linq;
 using System.Threading;
-using System.Threading.Tasks;
 using EnvDTE;
 using EnvDTE80;
 using Microsoft.Build.Execution;
