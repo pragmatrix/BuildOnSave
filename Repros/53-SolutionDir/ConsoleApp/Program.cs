f69d780408f0c24726747dc4dfb31738a5d3169e
Armin Sander <armin@replicator.org>
2018-11-01 20:31:19 +0100

    add a project that reproduces issue #53

---
+++ Repros/53-SolutionDir/ConsoleApp/Program.cs
@@ -0,0 +1,13 @@
+using System;
+using Test;
+
+namespace ConsoleApp
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine(typeof(TestClass).Name);
+		}
+	}
+}
---
+++ Repros/53-SolutionDir/Test.cs
@@ -0,0 +1,6 @@
+namespace Test
+{
+	static class TestClass
+	{
+	}
+}
